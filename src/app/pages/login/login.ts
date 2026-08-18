096ae7f20a2999ad4102de03c46e88fae6c2b364
Vitaliy Vynnyk <25266258+dedVitalik@users.noreply.github.com>
2026-08-18 17:55:55 +0200

    add set Bearer token (local storage)

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -4,6 +4,7 @@ import { Password } from 'primeng/password';
 import { Button } from 'primeng/button';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,6 +14,7 @@ import { AuthService } from '../../services/auth.service';
 })
 export class Login {
   authService = inject(AuthService);
+  route = inject(Router);
   private fb = inject(FormBuilder).nonNullable;
 
   loginForm = this.fb.group({
@@ -27,7 +29,9 @@ export class Login {
     }
     this.authService.login(this.loginForm.getRawValue())
       .subscribe(auth => {
-        console.log(auth);
+        this.authService.setToken(auth.token);
+        this.route.navigate(['/task-manager/board'])
+          .then();
       });
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,21 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   http = inject(HttpClient);
-  login(params: { user: string, password: string }) {
-    return this.http.post('/api/test/auth', params);
+  login(params: { user: string; password: string }): Observable<{token: string, user: string}> {
+    return this.http.post<{ token: string; user: string }>('/api/test/auth', params);
+  }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token') || null;
   }
 }
