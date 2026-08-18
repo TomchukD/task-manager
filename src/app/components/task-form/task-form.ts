781c9551f2e098e32f74e0dac5be1ee01f8672f9
Vitaliy Vynnyk <25266258+dedVitalik@users.noreply.github.com>
2026-08-18 18:33:46 +0200

    connect form

--- src/app/components/task-form/task-form.ts
+++ src/app/components/task-form/task-form.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { InputText } from 'primeng/inputtext';
 import { Textarea } from 'primeng/textarea';
 import { Button } from 'primeng/button';
 import { Select } from 'primeng/select';
 import { DatePicker } from 'primeng/datepicker';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'tm-task-form',
-  imports: [InputText, Textarea, Button, Select, DatePicker],
+  imports: [InputText, Textarea, Button, Select, DatePicker, ReactiveFormsModule],
   templateUrl: './task-form.html',
   styleUrl: './task-form.scss',
 })
-export class TaskForm {}
+export class TaskForm {
+  private fb = inject(FormBuilder);
+
+  taskForm = this.fb.group({
+    title: ['', Validators.required],
+    assignee: ['', Validators.required],
+    description: [],
+    status: ['', Validators.required],
+    priority: [],
+    deadline: [],
+  });
+}
