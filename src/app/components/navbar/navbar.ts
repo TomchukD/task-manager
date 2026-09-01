c6f67fe8d5104bfad4357fb98be52a19c39c6448
Vitaliy Vynnyk <25266258+dedVitalik@users.noreply.github.com>
2026-09-01 14:52:05 +0200

    wip: add assignee filter controls and edit mode label

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,18 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { Menubar } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
 import { DialogService } from 'primeng/dynamicdialog';
 import { TaskForm } from 'src/app/components/task-form/task-form';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Select } from 'primeng/select';
+import { UserRepo } from '../../shared/user.repo';
 
 @Component({
   selector: 'tm-navbar',
-  imports: [Menubar],
+  imports: [Menubar, ReactiveFormsModule, Select],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
   providers: [DialogService],
 })
 export class Navbar {
+  private fb = inject(FormBuilder);
+  private userRepo = inject(UserRepo);
   private dialogService = inject(DialogService);
+  public loading = this.userRepo.loading;
   public items: MenuItem[] = [
     {
       label: 'Home',
@@ -24,6 +30,12 @@ export class Navbar {
       command: () => this.openNewTask(),
     },
   ];
+
+  assigneeOptions = computed(() => ['All', ...this.userRepo.assigneeUser()]);
+
+  assigneeFilter = this.fb.group({
+    assignee: ['All'],
+  });
 
   private openNewTask(): void {
     this.dialogService.open(TaskForm, {
